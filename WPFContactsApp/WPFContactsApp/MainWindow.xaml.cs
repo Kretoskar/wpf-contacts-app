d20a3fbb2c9c11f706314d1f891a6772134226d2
Oskar <kogutoskar123@gmail.com>
2019-03-22 17:30:45 +0100

    Added windows flow

---
+++ WPFContactsApp/WPFContactsApp/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFContactsApp {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+        }
+
+        private void NewContactButton_Click(object sender, RoutedEventArgs e) {
+            ShowNewContactWindow();
+        }
+
+        /// <summary>
+        /// Opens up the window for adding a new contact
+        /// </summary>
+        private void ShowNewContactWindow() {
+            NewContactWindow newContactWindow = new NewContactWindow();
+            newContactWindow.ShowDialog();
+        }
+    }
+}
